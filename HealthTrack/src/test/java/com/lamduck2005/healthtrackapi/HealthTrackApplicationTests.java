ccbf75bf6217199fa5714595efe9dabff0a9297d
lamduck2005 <lampdph48614@gmail.com>
2025-11-11 18:35:51 +0700

    Init backend for project

---
+++ HealthTrack/src/test/java/com/lamduck2005/healthtrackapi/HealthTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamduck2005.healthtrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
